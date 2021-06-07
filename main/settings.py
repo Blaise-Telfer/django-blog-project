a627e5a625d1da033a0ec1eb314709cd9c3fc7b2
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-06-07 02:16:48 -0700

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -79,24 +79,12 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "DEMO_TEST",
-		"USER": "postgres",
-		"PASSWORD": "telfer12345",
-		"HOST": "localhost",
-		"PORT": "5432"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
